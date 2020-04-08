e053475d6f1cc00251bb8c0b58d5960849ee2a63
Spencer Moody <spencer.moody@outlook.com>
2020-04-08 10:46:55 -0700

    Add buisness interface for roll
    co-authored-by: Sean-Beloussow <seanbeloussow@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 // Buisiness Logic --------
+var turn = [];
+var total = [];
 
-var player1 = Player()
-var player2 = Player()
+var player1 = new Player("player1")
+var player2 = new Player("player2")
 
 function Player(name) {
   this.name = name;
   this.diceRoll = 0;
-  this.totalScore = 0;
+  this.roundScore = 0;
   
 }
 
-Player.prototype.roll = function()
- var roll = Math.floor((Math.random() * 6) + 1) ;
-console.log(roll)
+// Random number -------
+
+Player.prototype.roll = function() {
+var randomRoll = Math.floor((Math.random() * 6) + 1);
+
+  if (randomRoll != 1) {
+    turn.push(randomRoll); //need to determine when turn ends
+  } else {
+    turn = [0];
+    return "Rolled a 1!"
+    
+  }
+  return roll;
+}
